2e355c8ba1f27b87ac7d029cd94df34364b30ae7
Ashank586 <80754565+Ashank586@users.noreply.github.com>
2021-07-17 22:20:08 +0530

    Add files via upload

---
+++ login_page.js
@@ -0,0 +1,10 @@
+function enterplayer2name(){
+    window.location="login_page_2.html";
+}
+function storing(){
+    player1_name=document.getElementById("player1_name_input").value;
+    player2_name=document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("player_1_name",player1_name);
+    localStorage.setItem("player_2_name",player2_name);
+}
